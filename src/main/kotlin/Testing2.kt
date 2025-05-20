24fd81f2c247a2b71c4d0668d7b8b5c6d556bc66
obezeq <obezequiel@iesrafaelalberti.es>
2025-05-20 20:24:33 +0200

    Testing and getting ISSUES (need to solve)

--- src/main/kotlin/Testing2.kt
+++ src/main/kotlin/Testing2.kt
@@ -1,3 +1,20 @@
-fun main() {
+import data.DatabaseManager
+import data.dao.OperationDaoImpl
+import model.Operation
+import java.sql.SQLException
 
+fun main() {
+    val dbManager = DatabaseManager()
+    val dao = OperationDaoImpl(dbManager)
+
+    val op = Operation(
+        operaction = "5 + 3",
+        resultado = 8.0
+    )
+
+    try {
+        dao.insertOperation(op)
+    } catch (e: SQLException) {
+        println("[-] Error esperado: ${e.message}")
+    }
 }
