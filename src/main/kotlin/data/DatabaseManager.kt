59f93189c9021591fb3757d3dfee0a98143b3838
obezeq <obezequiel@iesrafaelalberti.es>
2025-05-20 15:58:37 +0200

    DatabaseManager strating

--- src/main/kotlin/data/DatabaseManager.kt
+++ src/main/kotlin/data/DatabaseManager.kt
@@ -1,7 +1,30 @@
 package data
 
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
 class DatabaseManager {
     private val jdbcUrl = "jdbc:h2:./db/calcDB"
     private val user = "sa"
     private val password = ""
+
+    fun getConnection(): Connection {
+        return try {
+            Class.forName("org.h2.Driver")
+            DriverManager.getConnection(jdbcUrl, user, password)
+        } catch (e: Exception) {
+            throw SQLException("Error al conectar a H2: ${e.message}")
+        }
+    }
+
+    fun closeConnection(connection: Connection) {
+        try {
+            if (!connection.isClosed) {
+                connection.close()
+            }
+        } catch (e: SQLException) {
+            System.err.println("[-] Error al cerrar conexión: ${e.message}")
+        }
+    }
 }
